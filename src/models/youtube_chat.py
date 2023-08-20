9604dceef709927f0d3dbb07351cb0df6647608a
omid-sar <mr.omid.sardari@gmail.com>
2023-08-20 18:24:29 -0400

    Second Commit

--- src/models/youtube_chat.py
+++ src/models/youtube_chat.py
@@ -7,12 +7,14 @@ from langchain.vectorstores import Chroma
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+import textwrap
 
 
 # Load  OpenAI API key from .env file and set it as the API key
 openai_key = os.getenv("OPENAI_KEY")
 os.environ["OPENAI_API_KEY"] = openai_key
 
+# *** YOUR VIDEO URL ***
 video_url = "https://www.youtube.com/watch?v=jGwO_UgTS7I"
 persist_directory = "../../data/processed"
 embeddings = OpenAIEmbeddings()
@@ -29,38 +31,40 @@ vectordb = Chroma.from_documents(
 )
 
 
-# Create a database of the youtube video url
-def youtube_url_db(video_url):
-    loader = YoutubeLoader.from_youtube_url(video_url)
-    transcript = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
-    docs = text_splitter.split_documents(transcript)
-    vectordb = Chroma.from_documents(
-        documents=docs, embedding=embeddings, persist_directory=persist_directory
-    )
-    return vectordb
-
-
-question = "What is course about?"
+# *** YOUR QUESTION ABUT THE VIDEO ***
+question = "what did course instructor say about computer learning?"
 
 docs = vectordb.similarity_search(query=question, k=3)
 docs_page_content = " ".join([doc.page_content for doc in docs])
+docs[0].page_content
 
-llm = OpenAI(temperature=0)
 template = """
 You are a knowledgeable assistant proficient in answering queries related
 to YouTube videos by analyzing their transcripts.
 
-Please address the query: {question}
+Please address the question: {question}
 By referencing the provided video transcript: {docs_page_content}
 
 If the answer isn't apparent in the transcript, kindly mention that you're unsure.
-Please refrain from speculating. Conclude all responses with "Thanks for asking!"""
+"""
+
+template = """ You are a helpful assistant that that can answer questions about youtube videos 
+        based on the video's transcript.
+        
+        Please address the question: {question}
+        By referencing the provided video transcript: {docs_page_content}   
+        
+        Only use the factual information from the transcript to answer the question.
+        
+        If you feel like you don't have enough information to answer the question, say "I don't know".
+        
+        Your answers should be verbose and detailed."""
 prompt = PromptTemplate(
     input_variables=["question", "docs_page_content"],
     template=template,
 )
 
+llm = OpenAI(temperature=0)
 chain = LLMChain(llm=llm, prompt=prompt)
 response = chain.run(question=question, docs_page_content=docs_page_content)
-print(response)
+print(textwrap.fill(response, width=85))
