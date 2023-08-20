59cf5c5b8d77daef861c3e3f4d23738f7d463793
omid-sar <mr.omid.sardari@gmail.com>
2023-08-20 18:05:34 -0400

    Second Commit

--- src/models/youtube_chat.py
+++ src/models/youtube_chat.py
@@ -6,6 +6,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 
 # Load  OpenAI API key from .env file and set it as the API key
@@ -13,7 +14,7 @@ openai_key = os.getenv("OPENAI_KEY")
 os.environ["OPENAI_API_KEY"] = openai_key
 
 video_url = "https://www.youtube.com/watch?v=jGwO_UgTS7I"
-persist_directory = '../../data/processed'
+persist_directory = "../../data/processed"
 embeddings = OpenAIEmbeddings()
 
 
@@ -21,38 +22,45 @@ loader = YoutubeLoader.from_youtube_url(video_url)
 transcript = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
 docs = text_splitter.split_documents(transcript)
-#docs[0].page_content
+# docs[0].page_content
 
-vectordb = Chroma.from_documents(documents=docs,
-                                 embedding=embeddings,
-                                 persist_directory=persist_directory)
+vectordb = Chroma.from_documents(
+    documents=docs, embedding=embeddings, persist_directory=persist_directory
+)
+
 
 # Create a database of the youtube video url
 def youtube_url_db(video_url):
     loader = YoutubeLoader.from_youtube_url(video_url)
     transcript = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
     docs = text_splitter.split_documents(transcript)
-    vectordb = Chroma.from_documents(documents=docs,
-                                 embedding=embeddings,
-                                 persist_directory=persist_directory)
+    vectordb = Chroma.from_documents(
+        documents=docs, embedding=embeddings, persist_directory=persist_directory
+    )
     return vectordb
 
 
-question = "What is the best way to learn a new language?"
+question = "What is course about?"
 
 docs = vectordb.similarity_search(query=question, k=3)
 docs_page_content = " ".join([doc.page_content for doc in docs])
 
 llm = OpenAI(temperature=0)
 template = """
-You are a knowledgeable assistant proficient in answering queries related to YouTube \n 
-videos by analyzing their transcripts.
+You are a knowledgeable assistant proficient in answering queries related
+to YouTube videos by analyzing their transcripts.
 
 Please address the query: {question}
-By referencing the provided video transcript: {docs}
+By referencing the provided video transcript: {docs_page_content}
 
-If the answer isn't apparent in the transcript, kindly mention that you're unsure. \n
-Please refrain from speculating. Conclude all responses with "Thanks for asking!""""
-prompt = PromptTemplate(input_variables=["docs_page_content", question], template=template)
+If the answer isn't apparent in the transcript, kindly mention that you're unsure.
+Please refrain from speculating. Conclude all responses with "Thanks for asking!"""
+prompt = PromptTemplate(
+    input_variables=["question", "docs_page_content"],
+    template=template,
+)
 
+chain = LLMChain(llm=llm, prompt=prompt)
+response = chain.run(question=question, docs_page_content=docs_page_content)
+print(response)
