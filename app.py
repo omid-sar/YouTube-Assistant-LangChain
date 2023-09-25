be1180cc21ddd650191a63c9a41dd8f4249a10ed
omid-sar <mr.omid.sardari@gmail.com>
2023-09-25 13:08:51 -0400

    Modifying code structure

--- app.py
+++ app.py
@@ -11,21 +11,37 @@ import textwrap
 import streamlit as st
 from apikey import apikey
 
-# Load  OpenAI API key from .env file and set it as the API key
+# Load  OpenAI API key and define directory to store the data
 os.environ["OPENAI_API_KEY"] = apikey
-
-# setup streamlit
-st.title("YouTube Video Transcript Analyzer")
-
-# Input for video URL
-
-# *** YOUR VIDEO URL ***
-video_url = st.text_input("Enter the YouTube video URL:")
 persist_directory = "../../data/processed"
+
+# Load the OpenAI Embeddings, LLM and PromptTemplate
 embeddings = OpenAIEmbeddings()
 llm = OpenAI(temperature=0)
-# when user enter the url, we will load the transcript and display it
-if video_url:
+
+template = """You can provide answers about YouTube videos using their transcripts.
+
+    For the question: {question}
+    Please refer to the video transcript: {docs_page_content}
+
+    Rely solely on the transcript's factual data to respond.
+
+    If the information isn't sufficient, simply state "I don't know".
+
+    Ensure your answers are comprehensive and in-depth.
+    """
+prompt = PromptTemplate(
+    input_variables=["question", "docs_page_content"],
+    template=template,
+)
+# Setup streamlit
+st.title("YouTube Video Transcript Analyzer")
+# *** YOUR VIDEO URL and QUESTION ***
+video_url = st.text_input("Enter the YouTube video URL:")
+question = st.text_input("Enter your question about the video:")
+
+#
+if video_url and question:
     loader = YoutubeLoader.from_youtube_url(video_url, add_video_info=True)
     info = loader._get_video_info()
     st.write("**Title:**", info["title"])
@@ -39,33 +55,12 @@ if video_url:
         documents=docs, embedding=embeddings, persist_directory=persist_directory
     )
 
+    # *** YOUR QUESTION ABUT THE VIDEO ***
 
-# *** YOUR QUESTION ABUT THE VIDEO ***
-question = st.text_input("Enter your question about the video:")
-if st.button("Analyze"):
     docs = vectordb.similarity_search(query=question, k=3)
     docs_page_content = " ".join([doc.page_content for doc in docs])
     docs[0].page_content
 
-    template = """You can provide answers about YouTube videos using their transcripts.
-
-    For the question: {question}
-    Please refer to the video transcript: {docs_page_content}
-
-    Rely solely on the transcript's factual data to respond.
-
-    If the information isn't sufficient, simply state "I don't know".
-
-    Ensure your answers are comprehensive and in-depth.
-    """
-
-    prompt = PromptTemplate(
-        input_variables=["question", "docs_page_content"],
-        template=template,
-    )
-
-    llm = OpenAI(temperature=0)
-    chain = LLMChain(llm=llm, prompt=prompt)
     response = chain.run(question=question, docs_page_content=docs_page_content)
     print(textwrap.fill(response, width=85))
 
